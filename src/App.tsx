66d616d1f4ae1e84417f7033b0db16eaae39901c
SantiagoYpes <santiyezu8@hotmail.com>
2025-04-19 16:18:00 -0500

    Niata e-commerce

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-
-
+import { useState } from "react"
+import Header from "./components/Header"
+import ProductList from "./components/ProductList"
+import Cart from "./components/Cart"
+import Footer from "./components/Footer"
 function App() {
-
-  return (
+  const [viewCart, setViewCart] = useState<boolean>(false)
+  const pageContent = viewCart ? <Cart/> : <ProductList/>
+  const content = (
     <>
-    
+      <Header viewCart ={viewCart} setViewCart = {setViewCart}/>
+      {pageContent}
+      <Footer viewCart = {viewCart}/>
     </>
   )
+  return content
 }
 
 export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { Nav } from "./Nav"
+import useCart from "../hooks/useCart"
+type PropsType ={
+    viewCart: boolean,
+    setViewCart: React.Dispatch<React.SetStateAction<boolean>>
+}
+const Header = ({viewCart, setViewCart}:PropsType) => {
+    const {totalItems, totalPrice} = useCart()
+    const content = (
+        <header className="header">
+            <div className="header_title-bar">
+                <h1>NIATA E-COMMERCE</h1>
+                <div className="header_price_box">
+                    <p>Total Items: {totalItems}</p>
+                    <p> Total Price: {totalPrice}</p>
+                </div>
+            </div>
+            <Nav viewCart={viewCart} setViewCart={setViewCart}/>
+        </header>
+    )
+    return content
+}
+
+export default Header
